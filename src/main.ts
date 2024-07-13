2c1fcdb6116e94f76dced06d21f37ebf0e68aa16
Yosiak-alv <Josias_romero60@outlook.com>
2024-07-12 23:08:54 -0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { useContainer } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // this will remove any properties that are not in the DTO
+    //forbidNonWhitelisted: true, // this will throw an error if there are any non-whitelisted properties in the request body
+  }));
+  
+  useContainer(app.select(AppModule), { fallbackOnErrors: true }); // add this line for registering the class-validator with the NestJS container
+
   await app.listen(3000);
 }
 bootstrap();
