ee2977731e5d704c0122b4f0fc1694a202039b91
Yosiak-alv <Josias_romero60@outlook.com>
2024-07-13 00:23:37 -0600

    adding transform
     to not show user in tasks index, show etc

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { useContainer } from 'class-validator';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,8 @@ async function bootstrap() {
     //forbidNonWhitelisted: true, // this will throw an error if there are any non-whitelisted properties in the request body
   }));
   
+  app.useGlobalInterceptors(new TransformInterceptor());
+
   useContainer(app.select(AppModule), { fallbackOnErrors: true }); // add this line for registering the class-validator with the NestJS container
 
   await app.listen(3000);
--- src/tasks/models/task.entity.ts
+++ src/tasks/models/task.entity.ts
@@ -1,5 +1,7 @@
-import { Entity, Column, PrimaryGeneratedColumn, BaseEntity } from "typeorm";
+import { Entity, Column, PrimaryGeneratedColumn, BaseEntity, ManyToMany, ManyToOne } from "typeorm";
 import { TaskStatus } from "../types/task-statuses.enum";
+import { User } from "src/auth/models/user.entity";
+import { Exclude } from "class-transformer";
 
 //Other Approach Active Record Pattern
 @Entity('tasks')
@@ -15,4 +17,8 @@ export class Task{
     
     @Column()
     status: TaskStatus;
+
+    @ManyToOne((_type) => User, user => user.tasks, {eager: false}) // eager false means that when we fetch a task, we don't fetch the user associated with that task
+    @Exclude({toPlainOnly: true}) // This will exclude the user when we return the task
+    user: User;
 }
