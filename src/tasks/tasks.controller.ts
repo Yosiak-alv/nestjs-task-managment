6598225101f15c18e3de90310848d9b4c7147c94
Yosiak-alv <Josias_romero60@outlook.com>
2024-07-13 00:24:35 -0600

    Final Changes

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -5,30 +5,50 @@ import { Task } from './models/task.entity';
 import { UpdateStatusTaskDto } from './dto/update-task.dto';
 import { GetTaskFilterDto } from './dto/get-task-filter.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from 'src/auth/decorators/get-user.decorators';
+import { User } from 'src/auth/models/user.entity';
+import { ApiResponse } from 'src/common/interfaces/api-response.interface';
 
 @Controller('tasks')
-@UseGuards(AuthGuard())
+@UseGuards(AuthGuard()) // This will protect all the routes in this controller
 export class TasksController {
     constructor(private readonly tasksService: TasksService) {}
 
     @Get()
-    async index(@Query() filterDto: GetTaskFilterDto) : Promise<Task[]> {
-        return await this.tasksService.findAll(filterDto);
+    async index(@Query() filterDto: GetTaskFilterDto, @GetUser() authUser: User) : Promise<ApiResponse> {
+        return {
+            data: await this.tasksService.findAll(filterDto, authUser),
+        };
     }
     @Get(':id')
-    async show( @Param('id') id: string ) : Promise<Task> {
-        return await this.tasksService.findById(id);
+    async show( @Param('id') id: string, @GetUser() authUser: User ) : Promise<ApiResponse> {
+        return {
+            data: await this.tasksService.findById(id, authUser),
+        }
     }
     @Post()
-    async create( @Body() createTaskDto : CreateTaskDto ) : Promise<Task> {
-        return await this.tasksService.create(createTaskDto);
+    async create( @Body() createTaskDto : CreateTaskDto , @GetUser() authUser: User) : Promise<ApiResponse> {
+        return {
+            data: await this.tasksService.create(createTaskDto, authUser),
+            message: 'Task created successfully',
+            statusCode: 201
+        }
     }
     @Patch(':id/status')
-    async updateStatus( @Param('id') id: string, @Body() updateStatusTaskDto : UpdateStatusTaskDto ) : Promise<Task> {
-        return await this.tasksService.updateStatus(id, updateStatusTaskDto);
+    async updateStatus( @Param('id') id: string, @Body() updateStatusTaskDto : UpdateStatusTaskDto,
+        @GetUser() authUser: User ) : Promise<ApiResponse> {
+        return {
+            data: await this.tasksService.updateStatus(id, updateStatusTaskDto, authUser),
+            message: 'Task status updated successfully',
+            statusCode: 200
+        }
     }
     @Delete(':id')
-    async destroy( @Param('id') id: string ): Promise<void> {
-        await this.tasksService.delete(id);
+    async destroy( @Param('id') id: string, @GetUser() authUser: User ): Promise<ApiResponse> {
+        await this.tasksService.delete(id, authUser);
+        return {
+            message: 'Task deleted successfully',
+            statusCode: 200
+        } 
     }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -6,6 +6,7 @@ import { UpdateStatusTaskDto } from './dto/update-task.dto';
 import { GetTaskFilterDto } from './dto/get-task-filter.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { User } from 'src/auth/models/user.entity';
 
 @Injectable()
 export class TasksService {
@@ -15,38 +16,39 @@ export class TasksService {
     ) {}
 
 
-    async findAll(filterDto: GetTaskFilterDto): Promise<Task[]> {
+    async findAll(filterDto: GetTaskFilterDto, authUser: User): Promise<Task[]> {
         const { status, search } = filterDto;
         const query = this.taskRepository.createQueryBuilder('task');
+        query.where({ user: authUser });
         if(status){
             query.andWhere('task.status = :status', {status});
         }
         if(search){
-            query.andWhere('LOWER(task.title) LIKE :search OR LOWER(task.description) LIKE :search ', 
+            query.andWhere('(LOWER(task.title) LIKE :search OR LOWER(task.description) LIKE :search)', 
                 {search: `%${search.toLocaleLowerCase()}%`});
         }
         return await query.getMany();
     }
 
-    async findById(id: string): Promise<Task> {
-        const task = await this.taskRepository.findOneBy({id});
+    async findById(id: string, authUser: User): Promise<Task | null> {
+        const task = await this.taskRepository.findOne({where: {id, user: { id: authUser.id }}});
         if(!task) throw new NotFoundException(`Task with id ${id} not found`);
         return task;
     }
 
-    async create(createTaskDto: CreateTaskDto): Promise<Task> {
-        const task = this.taskRepository.create({...createTaskDto, status: TaskStatus.OPEN});
+    async create(createTaskDto: CreateTaskDto, authUser: User): Promise<Task> {
+        const task = this.taskRepository.create({...createTaskDto, status: TaskStatus.OPEN, user: authUser});
         return await this.taskRepository.save(task);
     }
 
-    async updateStatus(id: string, updateStatusTaskDto: UpdateStatusTaskDto): Promise<Task> {
-        const task = await this.findById(id);
+    async updateStatus(id: string, updateStatusTaskDto: UpdateStatusTaskDto, authUser: User): Promise<Task> {
+        const task = await this.findById(id, authUser);
         task.status = updateStatusTaskDto.status;
         return await this.taskRepository.save(task);
     }
 
-    async delete(id: string): Promise<void> {
-        const result = await this.taskRepository.delete(id);
+    async delete(id: string, authUser: User): Promise<void> {
+        const result = await this.taskRepository.delete({id, user: { id: authUser.id }});
         if(result.affected === 0) throw new NotFoundException(`Task with id ${id} not found`);
         /* const task = await this.findById(id);
         await this.taskRepository.remove(task); remove expect a object */
